91bb0ea5ebec525346bf956084e9fc6db19ae34b
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-03-18 11:07:25 -0400

    feat: adicionando eslint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express from "express"
+import express from "express";
 
 export const app = express();
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,5 @@
-import { config } from "dotenv";
 import { app } from "..";
-config()
 
-const port= process.env.PORT
+const port = process.env.PORT;
 
-app.listen(port, () => console.log(`listening on port ${port}`))
+app.listen(port, () => console.log(`listening on port ${port}`));
