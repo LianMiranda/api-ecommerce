9011fb2aa29cfa73bc9b24272b84d49c4a61fadc
Lian Souza Miranda Mendes <lianmiranda26@gmail.com>
2025-04-01 14:41:20 -0400

    test: adjusting deleteUser test to work with new error handling method

--- src/api/useCases/user/Delete/DeleteUser.spec.ts
+++ src/api/useCases/user/Delete/DeleteUser.spec.ts
@@ -3,48 +3,48 @@ import { DeleteUserUseCase } from "./DeleteUserUseCase";
 import { User } from "../../../entities/User/User";
 
 const mockUserRepository = {
-    create: vi.fn(),
-    findAll: vi.fn(),
-    findById: vi.fn(),
-    findByEmail: vi.fn(),
-    update: vi.fn(),
-    delete: vi.fn(),
-  };
-  
-  const userDeleted = new User({
-      fullName: "John Doeeeeee",
-      email: "johnnnnnnnn@gmail.com",
-      password: "12345",
-      cpf: "98548788054",
-      birthday: "2000-01-01",
-      profileId: "1",
-  },"sdada6s45d654ad465sa456d645a")
-    
+  create: vi.fn(),
+  findAll: vi.fn(),
+  findById: vi.fn(),
+  findByEmail: vi.fn(),
+  update: vi.fn(),
+  delete: vi.fn(),
+};
+
+const userDeleted = new User(
+  {
+    fullName: "John Doeeeeee",
+    email: "johnnnnnnnn@gmail.com",
+    password: "12345",
+    cpf: "98548788054",
+    birthday: "2000-01-01",
+    profileId: "1",
+  },
+  "sdada6s45d654ad465sa456d645a"
+);
+
 describe("Delete User", () => {
-    const deleteUser = new DeleteUserUseCase(mockUserRepository);
+  const deleteUser = new DeleteUserUseCase(mockUserRepository);
 
-    beforeEach(() => {
-        vi.resetAllMocks();
-    })
+  beforeEach(() => {
+    vi.resetAllMocks();
+  });
 
-    it("should be a possible delete user successfully", async () => {
-        mockUserRepository.findById.mockResolvedValue(userDeleted);
-        mockUserRepository.delete.mockResolvedValue(userDeleted);
+  it("should be a possible delete user successfully", async () => {
+    mockUserRepository.findById.mockResolvedValue(userDeleted);
+    mockUserRepository.delete.mockResolvedValue(userDeleted);
 
-        const response = await deleteUser.delete("sdada6s45d654ad465sa456d645a");
-        
-        expect(response.body).toBeInstanceOf(User);
-        expect(response.StatusCode).toBe(200)
-        expect(response.message).toBe("Usuário deletado com sucesso")
-    });
+    const response = await deleteUser.delete("sdada6s45d654ad465sa456d645a");
 
-    it("should not be possible to delete user that does not exist", async () => {
-        mockUserRepository.findById.mockResolvedValue(null);
+    expect(response.body).toBeInstanceOf(User);
+    expect(response.StatusCode).toBe(200);
+    expect(response.message).toBe("Usuário deletado com sucesso");
+  });
 
-        const response = await deleteUser.delete("12333");
-
-        expect(response.body).toEqual({});
-        expect(response.message).toBe("Nenhum usuário encontrado")
-        expect(response.StatusCode).toBe(404)
-    })
-})
+  it("should not be possible to delete user that does not exist", async () => {
+    mockUserRepository.findById.mockResolvedValue(null);
+    await expect(deleteUser.delete("12333")).rejects.toThrow(
+      "Nenhum usuário encontrado"
+    );
+  });
+});
